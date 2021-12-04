d9ead8bae6d33ea9d969ae584666146a761cc994
notrealchauloan <75651801+notrealchauloan@users.noreply.github.com>
2021-12-04 23:52:05 +0700

    Function get_user (for posts)

--- classes/login.class.php
+++ classes/login.class.php
@@ -22,7 +22,8 @@ class User extends Database{
         }
     }
  
-    public function details($sql){
+    public function details($id){
+        $sql = "SELECT * FROM users WHERE userid = '$id'";
  
         $query = $this->connection->query($sql);
  
@@ -34,6 +35,22 @@ class User extends Database{
     public function escape_string($value){
  
         return $this->connection->real_escape_string($value);
+    }
+
+    public function get_user($id)
+    {
+        $query = "SELECT * FROM users WHERE userid = '$id' LIMIT 1";
+        $DB = new Database();
+        $result = $DB->read($query);
+
+        if($result)
+        {
+            return $result[0];
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
