4c45acd74f5ab2ca7697f4fd34c62bf1f6d4c22c
notrealchauloan <75651801+notrealchauloan@users.noreply.github.com>
2021-12-05 00:19:31 +0700

    Universal Page Header

--- index.php
+++ index.php
@@ -52,26 +52,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <nav>
-        <div class="container">
-            <h2 class="log">
-                <a href="index.php">socialBook</a>
-            </h2>
-            <div class="search-bar">
-                <i class="uil uil-search"></i>
-                <input type="search" placeholder="Search for creators, inspirations, and projects">
-            </div>
-            <div class="create">
-                <label class="btn btn-primary" for="create-post">Create</label>
-                <div class="profile-photo">
-                    <a href="profile.php"><img src="./images/profile-1.jpg"></a>
-                </div>
-                <label class="btn btn-primary" for="sign-out">
-                    <a style="color:#fff;" href="logout.php">Sign Out</a>    
-                </label>
-            </div>
-        </div>
-    </nav>
+    <?php
+        include('header.php');
+    ?>
 
     <!------------------------- MAIN -------------------------->
     <main>
--- profile.php
+++ profile.php
@@ -55,28 +55,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 </head>
 
 <body>
-    <nav>
-        <!------------------- NAVBAR --------------------->
-        <div class="container">
-            <h2 class="log">
-                <a href="index.php">socialBook</a>
-            </h2>
-            <div class="search-bar">
-                <i class="uil uil-search"></i>
-                <input type="search" placeholder="Search for creators, inspirations, and projects">
-            </div>
-            <div class="create">
-                <label class="btn btn-primary" for="create-post">Create</label>
-                <div class="profile-photo">
-                    <a href="profile.php"><img src="./images/profile-1.jpg"></a>
-                </div>
-                <label class="btn btn-primary" for="sign-out">
-                <a style="color:#fff;" href="logout.php">Sign Out</a>    
-            </label>
-            </div>
-        </div>
-    </nav>
-
+    <?php
+        include('header.php');
+    ?>
     <div class="container db-social">
         <div class="jumbotron jumbotron-fluid"></div>
         <div class="container">
