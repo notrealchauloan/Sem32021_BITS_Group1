f2539790a982f125b302ed81dcff7a2002a63f15
tnguy753 <nguyenthao989@gmail.com>
2021-12-11 20:37:59 +0700

    Finalize UI for Therapy Page

--- help.php
+++ help.php
@@ -35,10 +35,11 @@ $user_details = $user->get_user($id);
 	width:100%;
 	border-top: 1px solid rgba(58,58,58,0.03);
 	border-bottom: 1px solid rgba(58,58,58,0.03);
-	padding: 50px 0px;
+	padding: 0px 0px;
 	background-image: url("../images/bg.png");
 	background-position: center;
 	background-size: 1000px;
+    background-color: Hazelnut;
 }
 .therapist-heading{
 	display: flex;
@@ -51,7 +52,7 @@ $user_details = $user->get_user($id);
 	justify-content: center;
 	align-items: center;
 	flex-wrap: wrap;
-	margin:20px 0px;
+	margin:10px 0px;
 }
 .therapist-box {
 	width:310px;
@@ -100,35 +101,101 @@ display: flex;
 	text-overflow: ellipsis;
 }
 .therapist-box a{
-	width:190px;
-	height: 44px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	color:#FFFFFF;
-	background-color: #1db096;
-	border-radius: 20px;
-	box-shadow: 4px 10px 30px rgba(24,139,119,0.2);
+display: inline-block;
+  padding: var(--btn-padding);
+  font-weight: 500;
+  border-radius: var(--border-radius);
+  cursor: pointer;
+  transition: all 300ms ease;
+  font-size: 0.9rem;
 }
-.therapist-box a{
-	width:140px;
-	margin-top: 20px;
-	border-radius: 0px;
-}
+
 .therapist-box:hover{
 	transform: translateY(-10px);
 	box-shadow: 5px 10px 30px rgba(0,0,0,0.1);
     background-color: #23cdaf;
 	transition: all ease 0.2s;
 }
-.container {
-  width: 80%;
-  margin: 0 auto;
+.word {
+  font-size: 20px;
+  margin: 25px;
+  width: 600px;
+  height: 100px;
 }
+#heading,contact{
+	display:flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin:70px 4px;
+    background-color: yellow;
+}
+h3{
+	font-size:4rem;
+	margin:20px;
+    text-align: center;
+}
+#heading img{
+	max-width: 100%;
+    height: auto;
+}
+#contact{
+	display:flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin:40px 0px;
+}
+#contact h3{
+	font-size:2.5rem;
+	margin:20px;
+}
+.contact-input{
+	width:400px;
+	height:50px;
+	background-color:#FFFFFF;
+	display:flex;
+	justify-content: center;
+	border-radius: 50px;
+	box-shadow: 2px 2px 30px rgba(0,0,0,0.15);
+}
+.contact-input input{
+	width:100%;
+	background-color: transparent;
+	border:none;
+	outline: none;
+	text-align: center;
+	color:#242425;
+}
+.contact-input a{
+	width:200px;
+	height:35px;
+	background-color:#1db096;
+	color:#FFFFFF;
+	display: flex;
+	font-size: 0.9rem;
+	justify-content: center;
+	align-items: center;
+	margin: auto 10px;
+	border-radius: 20px;
+	font-weight: 500;
+}
+footer{
+	display:flex;
+	justify-content: space-around;
+	border-top: 1px solid rgba(232,232,232,0.5);
+	background-color:#FFFFFF;
+}
+footer p{
+	margin:15px 0px;
+	color:#5f5f5f;
+	font-size: 0.9rem;
+}
+
 </style>
 
 </head>
-<body>
+<body background="./images/welcome.png">
 <?php
     include('header.php');
     $profile_image = "";
@@ -138,10 +205,13 @@ display: flex;
         $image = $user_details['profile_image'];
     }
 ?>
-<main>
-<div class="middle">
-    <h1> Don't worry! We're here for you! </h1>
-</main>
+<!--heading------------------------->
+<section id="heading">
+	<!--img-------->
+	<img src="https://images.unsplash.com/photo-1620389701363-b1d7a601e0c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
+</section>
+  <!--heading---------------->
+	<h3>Professional, licensed, and vetted therapists who you can trust</h3>
 
   <!--therapist----------------------------------->
   <section id="therapist">
@@ -166,7 +236,9 @@ display: flex;
 		<!--details/comment------>
 		<p>Hi. My name is Lynda. I am a Person-Centred Counsellor (MNCS ProfAccred) providing one to one counselling sessions for adults, couples and children.  Counselling is not about me offering you advice or direction, but about me supporting you to find the solutions that fit you as an individual, helping you move forward with clarity and new insights towards a positive outcome.</p>
 		<!--btn--------->
-		<a href="#">Appointment</a>
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
 	</div>
 		<!--box-2----->	
 	<div class="therapist-box">
@@ -178,12 +250,14 @@ display: flex;
 			<i class="fas fa-star"></i>
 			<i class="fas fa-star"></i>
 			<i class="fas fa-star"></i>
-			<i class="far fa-star"></i>
+			<i class="fas fa-star"></i>
 		</div>
 		<!--details/comment------>
 		<p>Do you feel depressed or anxious and don't know what to do to feel well again? Are you in a controlling relationship and don't know what to do next? Do you want to be you again, be a happy and respected person? I am a counsellor helping you to improve your mental health by finding new coping strategies. Together we will explore the underlying reasons of your emotional struggles.  </p>
 		<!--btn--------->
-		<a href="#">Appointment</a>
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
 	</div>
 		<!--box-3----->	
 	<div class="therapist-box">
@@ -194,15 +268,131 @@ display: flex;
 			<i class="fas fa-star"></i>
 			<i class="fas fa-star"></i>
 			<i class="fas fa-star"></i>
-			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
 			<i class="far fa-star"></i>
 		</div>
 		<!--details/comment------>
 		<p>I specialise in the Person-Centred Approach. There are no particular areas or problems in which I focus my work. I specialise in creating an environment which encourages and develops personal growth.
 If you feel that you may benefit from counselling but are concerned about costs, please do not let this deter you.</p>
 		<!--btn--------->
-		<a href="#">Appointment</a>
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
+	</div>
+    <!--box-4----->	
+	<div class="therapist-box">
+		<!--img-------->
+		<img src="./images/therapist-8.jpeg"/>
+		<!--stars/reviews---------->
+		<div class="star">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
+		</div>
+		<!--details/comment------>
+		<p>We all struggle to make sense of experiences at times, for example, when our life is in transition as we are leaving home or taking on new caring responsibilities. As a counsellor I can offer the opportunity to talk in confidence. I have worked in mental health for over 20 years and have trained as a social worker as well as a counsellor.   </p>
+		<!--btn--------->
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
+	</div>
+		<!--box-5----->	
+	<div class="therapist-box">
+		<!--img-------->
+		<img src="./images/therapist-6.jpeg"/>
+		<!--stars/reviews---------->
+		<div class="star">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
+			<i class="far fa-star"></i>
+		</div>
+		<!--details/comment------>
+		<p>I am an Integrative counsellor, using a range of approaches to help you individually. I have training in gestalt and psychodynamic counselling, with both individuals and groups. Your view of the world is important in its impact on how you cope in life. I see counselling as a partnership. I am a qualified Equine Facilitated Psychotherapist..</p>
+		<!--btn--------->
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
+	</div>
+    <!--box-6----->	
+	<div class="therapist-box">
+		<!--img-------->
+		<img src="./images/therapist-4.jpeg"/>
+		<!--stars/reviews---------->
+		<div class="star">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
+		</div>
+		<!--details/comment------>
+		<p>I have experience working with young people both in an educational setting and as a Counsellor. I specifically have knowledge and experience of working with young people who have additional learning needs, such as Autism and ADHD. Areas of counselling I deal with include (but are not limited to); Anxiety, Stress, Exam stress, Depression, Trauma, etc...</p>
+		<!--btn--------->
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
+	</div>
+    <!--box-7----->	
+	<div class="therapist-box">
+		<!--img-------->
+		<img src="./images/therapist-7.jpeg"/>
+		<!--stars/reviews---------->
+		<div class="star">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
+			<i class="far fa-star"></i>
+			<i class="far fa-star"></i>
+		</div>
+		<!--details/comment------>
+		<p>Hi, my name is Emma and I am a qualified Person-centred counsellor (MBACP). Being Person-centred means that the sessions I offer are client-led, valuing the unique experience of each individual. We can all face times in our lives where we feel like we need extra support or just a listening ear.</p>
+		<!--btn--------->
+		<a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
+	</div>
+    <!--box-8----->	
+	<div class="therapist-box">
+		<!--img-------->
+		<img src="./images/therapist-5.jpeg"/>
+		<!--stars/reviews---------->
+		<div class="star">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="far fa-star"></i>
+			<i class="far fa-star"></i>
+		</div>
+		<!--details/comment------>
+		<p>If you feel like you need someone to talk to or a safe space to explore your thoughts and feelings without being judged then please get in heading with me and we can arrange a session to begin your therapeutic journey.</p>
+		<!--btn--------->
+		<a href="#"> 
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Book an Appointment</button>
+        </a>
 	</div>
 	</div>
 	</section>
+</br>
+<!--contact------------------------->
+<section id="contact">
+	<!--heading---------------->
+	<h3>Stay Connected</h3>
+	<!--input----------------->
+	<div class="contact-input">
+		<input type="email" placeholder="Example@gmail.com"/>
+        <a href="#">
+        <button type="button" style="float: right; font-weight:bold" class="btn btn-primary">Continue</button>
+        </a>
+	</div>
+	</section>
+	<!--footer--------->
+	<footer>
+	<p>Going To Internet, Ltd Consumer Website</p>
+	<p>Copyright 2020 - GoingToInternet</p>
+	</footer>
+
 
