77e5dd192327a26cc3f4ea9be7bbc0b22284fdfb
notrealchauloan <75651801+notrealchauloan@users.noreply.github.com>
2021-12-14 21:03:08 +0700

    Post with image

--- classes/post.class.php
+++ classes/post.class.php
@@ -1,5 +1,5 @@
 <?php
-class Post
+class Post 
 {
     private function create_postid()
     {
@@ -16,17 +16,39 @@ class Post
 
     private $error = "";
 
-    public function create_post($userid, $data)
-    {
-        // $content = $data['post'];
-        
+    public function create_post($userid, $data, $files)
+    {     
         // verify the data
-        if(!empty($data['post']))
+        if(!empty($data['post']) || !empty($files['file']['name']))
         {
+            $myimage = "";
+            $has_image = 0;
+
+            if(!empty($files['file']['name']))
+            {
+                // create folder to store images
+                $folder = "uploads/" . $userid . "/";
+
+                if(!file_exists($folder))
+                {
+                    mkdir($folder, 0777, true);
+                }
+
+                $image_class = new Image();
+
+                $myimage = $folder . $image_class->generate_filename(20) . ".jpg";
+
+                move_uploaded_file($_FILES['file']['tmp_name'],$myimage);  
+
+                $image_class->crop_image($myimage,$myimage,1000,300); 
+
+                $has_image = 1;
+            }
+
             $post = addslashes($data['post']);
             $postid = $this->create_postid();
 
-            $query = "INSERT INTO posts (userid, postid, content) VALUES ('$userid', '$postid', '$post')";
+            $query = "INSERT INTO posts (userid, postid, content, images, has_image) VALUES ('$userid', '$postid', '$post', '$myimage', '$has_image')";
             $DB = new Database();
             $DB->save($query);
         }
