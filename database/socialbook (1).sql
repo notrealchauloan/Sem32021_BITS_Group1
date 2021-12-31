80171de80889c44d08accb46235bbf603a85e55c
s3904419 <a8.phong06@gmail.com>
2021-12-31 22:09:50 +0700

    Update socialbook (1).sql

--- database/socialbook (1).sql
+++ database/socialbook (1).sql
@@ -127,7 +127,14 @@ INSERT INTO `chatbot` (`id`, `queries`, `replies`) VALUES
 (1, "hi|hello|hey|wassup|what's up", 'Hello there!'),
 (2, "how are you|how old are you|what's your name", "I am just a bot... I don't have an answer for that"),
 (3, "I am stressed|I am tired", "Tell me everything, I'm here for you~"),
-(4, "joke", "You are a joke.");
+(4, "joke", "You are a joke."),
+(5, "arrange|meeting|psychiatrist", "Please click the 'Professional Helps' tab to schedule a meeting!"),
+(6, "help", "You should talk to a psychiatrist for help."),
+(7, "fun|funny", "I am programmed to be funny."),
+(8, "time", "Look at the bottom right corner on your computer screen!"),
+(9, "dumb|stupid", "Why are you so mean?"),
+(10, "socialbook|social book|", "socialBook is a wonderful place where you are free to express your feelings, 
+and you can contact a professional to help with your emotions anytime!");
 
 --
 -- Indexes for table `posts`
